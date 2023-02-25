1187b14ec898b2281f6fcddef1717e1be216ac65
RakibHossainMorul <67518360+RakibHossainMorul@users.noreply.github.com>
2023-02-25 23:00:11 +0600

    updated

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:instagram_clone/resources/storage_methods.dart';
 
+//
+//
 //***This User Defined Method is resposible for Flutter Applicatin Authemtication
 // -part connected to the Firebase Authentication.
 //
